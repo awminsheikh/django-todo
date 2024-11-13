ff6392e760a586a0928904ee1232171f3e15ec1c
awminsheikh <amin.sheikhazimi2212gmail.com>
2024-11-13 22:57:45 +0330

    create firstly Components

---
+++ front-end/src/Components/Todoes/TodoContainer.tsx
@@ -0,0 +1,6 @@
+
+export const TodoContainer = () => {
+  return (
+    <div>TodoContainer</div>
+  )
+}
---
+++ front-end/src/Components/Todoes/todoList.ts
@@ -0,0 +1,16 @@
+const todoList = [
+    {
+        "id" : 1,
+        title: "Todo 1",
+        completed: false
+        
+    },
+    {
+        "id" : 2,
+        title: "Todo 2",
+        completed: true
+        
+    },
+]
+
+export default todoList;
